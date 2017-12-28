20de29122708adac2dee63f32fc375a70c2cac99
IwonaMaurer <plastimex2@wp.pl>
2017-12-28 13:57:44 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '66758145140-3oobdp5r73reqmm1t6bbi0fov2iaoal6.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'ABHuOBU_4f3RKXH-v1ZaYxzJ',
+    'CALLBACK-URL': 'http://localhost:3000/auth/google/callback'
+};
